66556a067f2f51f5a9af8bdd4ee230ea11b475c8
Gilbert Marín <daviddlioncourt@gmail.com>
2021-04-10 22:47:31 +0000

    Last Modeling

--- src/main.py
+++ src/main.py
@@ -8,13 +8,20 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Films, Favorites
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = os.environ.get('SALA_TRES')
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -39,7 +46,66 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+@app.route("/login", methods=["POST"])
+def login():
+    user = request.json.get("username", None)
+    pasw = request.json.get("password", None)
+    finduser= User.query.filter_by(username=user, password=pasw).first()
+    if finduser is None:
+        return jsonify({"msg": "Invalid Information"}), 401
+
+    access_token = create_access_token(identity=user)
+    return jsonify(access_token=access_token)
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():  
+    
+    return jsonify(People.getAllPeople()), 200
+
+#Start App routes for Favorites
+
+@app.route('/favorites/<int:id>', methods=['POST'])
+@jwt_required()
+def handle_favoritespost(id):  
+    
+    return jsonify(Favorites.getAllFavorites(id)), 200
+
+@app.route('/favorites/', methods=['GET'])
+@jwt_required()
+def handle_favoritesget():
+
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(username=current_user).first()
+    response = Favorites.query.filter_by(user_id=user.id)
+    all_favorites = list(map(lambda x: x.serialize(), response))
+    print(all_favorites)
+    return jsonify(all_favorites), 200
+
+#End App routes for Favorites
+
+@app.route('/planets', methods=['GET'])
+def handle_planets():  
+    
+    return jsonify(Planets.getAllPlanets()), 200
+
+@app.route('/films', methods=['GET'])
+def handle_films():  
+    
+    return jsonify(Films.getAllFilms()), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def handle_peopleid(id):  
+    
+    return jsonify(People.getPerson(id)), 200
+
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
--- src/models.py
+++ src/models.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    firstname = db.Column(db.String(15), nullable=False)
+    lastname = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(15), unique=True, nullable=False)    
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -14,6 +17,108 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email
+            #"favorites": list(map(lambda x: x.serialize(), self.favorites))           
         }
+        
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(28), nullable=False)
+    
+    def __repr__(self):
+        return '<People %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+            #"favorites": list(map(lambda x: x.serialize(), self.favorites))           
+        }
+    
+    def getAllPeople():
+        all_people = People.query.all()
+        all_people = list(map(lambda x: x.serialize(), all_people))
+        return all_people
+    
+    def getPerson(id):
+        person = People.query.get(id)
+        return person.serialize()
+
+class Favorites(db.Model):
+    __tablename__= 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(10), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"))
+    films_id = db.Column(db.Integer, db.ForeignKey("films.id"))
+    user= db.relationship('User', lazy=True)  
+    planets= db.relationship('Planets', lazy=True)  
+    people= db.relationship('People', lazy=True)  
+    films= db.relationship('Films', lazy=True)  
+
+    def serialize(self):
+        if self.people_id is not None:
+            value =  People.query.get(self.people_id).name
+        elif self.planets_id is not None:
+            value =  Planets.query.get(self.planets_id).name
+        elif self.films_id is not None:
+            value =  Films.query.get(self.films_id).title
+        return {
+            "id": self.id,
+            "date": self.date,
+            "name": value         
+        }
+    def getAllFavorites(id):
+        favorites = Favorites.query.get(id)        
+        favorites = list(map(lambda x: x.serialize(), favorites))
+        return favorites
+
+    def getAllFavoritesPost(id, postobj):
+        postfavorites = Favorites.query.get(id)
+
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(28), nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+            #"favorites": list(map(lambda x: x.serialize(), self.favorites))           
+        }
+    def getAllPlanets():
+        all_planets = Planets.query.all()
+        all_planets = list(map(lambda x: x.serialize(), all_planets))
+        return all_planets
+
+class Films(db.Model):
+    __tablename__ = 'films'
+    id = db.Column(db.Integer, primary_key=True)    
+    title = db.Column(db.String(100), nullable=False)  
+    director = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Films %r>' % self.title    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title
+            #"favorites": list(map(lambda x: x.serialize(), self.favorites))           
+        }
+    def getAllFilms():
+        all_films = Films.query.all()
+        all_films = list(map(lambda x: x.serialize(), all_films))
+        return all_films
